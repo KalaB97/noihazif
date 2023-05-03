cfeebb7bbb8c1f6b283eda6b88fe8b8d543d911f
unknown <szegedizs@kkszki.hu>
2023-05-03 12:42:10 +0200

    tesztkesz

--- teszt.py
+++ teszt.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.binary_location = r"C:/Program Files/Mozilla Firefox/firefox.exe"
-driver = webdriver.Firefox(options=options, executable_path="C:\Users\szege\OneDrive\Asztali gép\geckodriver\geckodriver")
+driver = webdriver.Firefox(options=options, executable_path=r"C:\Users\szege\OneDrive\Asztali gép\geckodriver\geckodriver")
 driver.get("http://127.0.0.1:5500/noi.html")
 time.sleep(3)
 driver.save_screenshot("noi.png")
